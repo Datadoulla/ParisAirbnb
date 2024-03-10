7d3084dfec522f4e65686259887d84e190aab0e0
datadoulla <datadoulla@gmail.com>
2024-03-10 03:40:28 +0100

    put euro symbole

--- src/tools/predict.py
+++ src/tools/predict.py
@@ -25,9 +25,9 @@ def model_predict(data, model_pkl='models/model_simple_2023.pkl'):
     # Prepare the results dictionary
     # Ensure necessary libraries like numpy (np) are imported
     srt = {
-    'predict': round(np.exp(srt_pre).iloc[0],2),
-    'born_inf': round(np.exp(srt_conf)[0][0],2),
-    'born_sup': round(np.exp(srt_conf)[0][1],2)
+    'predict': round(np.exp(srt_pre).iloc[0]*0.9372,2),
+    'born_inf': round(np.exp(srt_conf)[0][0]*0.9372,2),
+    'born_sup': round(np.exp(srt_conf)[0][1]*0.9372,2)
     }
 
     # Print the results
